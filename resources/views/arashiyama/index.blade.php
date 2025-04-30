e977d3f3cc09cfbacbc88c34ab4d5173d8f2805e
lasaya10 <saitosaya1015@gmail.com>
2025-04-30 14:16:03 +0000

    Display name column

---
+++ resources/views/arashiyama/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>嵐山マップ</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+
+<body>
+    <h1>嵐山マップ一覧</h1>
+
+    <ul>
+        @foreach ($places as $place)
+        <li>{{ $place->name }}</li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
